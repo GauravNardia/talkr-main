8a9ad1b07f7f0da1bdf1055a5767bf5a476f8463
Gaurav Nardia <gauravnardia07@gmail.com>
2025-04-12 18:28:44 +0530

    init prisma

---
+++ packages/database/src/client.ts
@@ -0,0 +1,7 @@
+import { PrismaClient } from "../generated/client";
+
+const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
+
+export const prisma =
+  globalForPrisma.prisma || new PrismaClient();
+
