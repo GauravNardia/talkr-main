2c1de88415b63164908cf44cc714255b2b754c8d
Gaurav Nardia <gauravnardia07@gmail.com>
2025-04-12 19:03:57 +0530

    fix db url 3

--- packages/database/src/client.ts
+++ packages/database/src/client.ts
@@ -1,17 +1,5 @@
 import { PrismaClient } from "../generated/client";
 
-declare global {
-  // Prevent multiple instances in dev
-  var prisma: PrismaClient | undefined;
-}
+const prisma = new PrismaClient()
 
-if (!process.env.DATABASE_URL) {
-  throw new Error("DATABASE_URL is missing in environment variables");
-}
-
-export const prisma =
-  global.prisma || new PrismaClient();
-
-if (process.env.NODE_ENV !== "production") {
-  global.prisma = prisma;
-}
+export default prisma
